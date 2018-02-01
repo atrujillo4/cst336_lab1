a53b30a1c96fa8885f4840df11709ae8a47883d5
Adrian Trujillo <atrujillo@csumb.edu>
2018-02-01 01:39:18 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8">
+        <title> Adrian Trujillo: Personal Website </title>
+        <style>
+           
+        </style>
+        
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+    
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body >
+        <header>
+            <h1> Adrian Trujillo </h1>
+        </header>
+        <nav>
+            <hr width="50%" />
+            <a href="index.html"><strong>Home</strong></a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /><br />
+        
+        <main>
+            
+            <figure id="me">
+                <img src="img/juan_doe.png" width="250" height="250" alt="Picture of Juan Doe" />
+                
+            </figure>
+            
+            <div id="welcomeText">
+                Hello! <br />
+                <p>Thank you for visiting my professional portfolio website.</p>
+                <p>I am a Software Engineer and have been recent hired at Apple.</p>
+                <p>Feel free to contact me!</p>
+                <br /><br />
+                <em>"With ordinary talent and extraoardinary <strong>perseverance</strong>, all things are attainable"</em><br />
+                
+                - Thomas F. Buxton
+                
+            </div>
+            
+        </main>
+        
+        
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST336 Internet Programming. 2018&copy; Trujillo <br />
+            <strong>Disclaimer:</strong>The information in this webpage is fictitous. <br />
+            It is used for academic purposes only.
+            <p> <img src="https://www.prepsportswear.com/media/images/college_logos/300x300/1749151_mktg_logo.png" width="100" height="100"></p>
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
